7dd2ede96d4a54f053edd6d3570bb3eef2001f52
fabiopina <fpina@student.dei.uc.pt>
2018-05-14 01:40:48 +0100

    finished aggregator ms

--- Aggregator_MS/business/aggregator_controller.py
+++ Aggregator_MS/business/aggregator_controller.py
@@ -2,6 +2,7 @@ import logging
 import business.response_handling as RESP
 import requests
 import os
+import json
 from business.auth import requires_auth
 from flask import request
 
@@ -26,9 +27,11 @@ def get_playlist_songs_info(id):
     # Checks if song exists by sending a request into the Songs Microservice
     headers = {'Content-Type': 'application/json',
                'Authorization': request.headers['Authorization']}
-    param = {'id': id}
-    print(request.headers['Authorization'])
-    r = requests.get(PLAYLISTS_MS + '/playlists', params=param, headers=headers)
+
+    base_url = PLAYLISTS_MS + '/playlists/songs'
+    url = '/'.join((base_url, str(id)))
+
+    r = requests.get(url, headers=headers)
     if r.status_code == 400:
         return RESP.response_400()
     if r.status_code == 404:
@@ -36,4 +39,16 @@ def get_playlist_songs_info(id):
     if r.status_code == 500:
         return RESP.response_500(message='Playlists_MS is down!')
 
-    return RESP.response_200()
+    response_data = json.loads(r.text)
+
+    result = []
+    for song in response_data:
+        param = {'id': song['song_id']}
+        r = requests.get(SONGS_MS + '/songs', params=param, headers=headers)
+        if r.status_code == 404:
+            return RESP.response_404(message='Song not found!')
+        if r.status_code == 500:
+            return RESP.response_500(message='Songs_MS is down!')
+        result.append(json.loads(r.text))
+
+    return RESP.response_200(message=result)
